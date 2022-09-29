bde7adf7c9012be38aed0a4fb4112485520e6ba0
Vinicius Zucatti <viniciuszucatti@gmail.com>
2022-09-29 19:24:42 -0300

    Create signin and singup form

--- apps/client/features/auth/index.tsx
+++ apps/client/features/auth/index.tsx
@@ -1,7 +1,12 @@
-import { FormEvent, useCallback } from "react";
-import { GridMain, Heading, media } from "sagu-ui/lib";
+import { FormEvent, useCallback, useState } from "react";
+import { GridMain, Heading, Radio } from "sagu-ui/lib";
 import styled, { css } from "styled-components";
 import Form from "./Form";
+
+enum TypeOfForms {
+  Register,
+  Login,
+}
 
 const Wrapper = styled.div`
   ${({ theme }) => css`
@@ -16,44 +21,116 @@ const Wrapper = styled.div`
   `}
 `;
 
-const FormGroup = styled(Wrapper)`
-  ${media.sm`
-    grid-template-columns: 1fr 1fr;  
+const RadioGroup = styled.div`
+  ${({ theme }) => css`
+    grid-template-columns: 1fr 1fr;
+    grid-gap: ${theme.spacings.xsmall};
   `}
 `;
 
+const ErrorMessage = styled.b`
+  ${({ theme }) => css`
+    color: ${theme.colors.base.alert};
+  `}
+`;
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export default function Auth() {
-  const handleSignIn = useCallback((e: FormEvent<HTMLFormElement>) => {
+  const [switchForm, setSwitchForm] = useState<TypeOfForms>(TypeOfForms.Login);
+  const [error, setError] = useState("");
+
+  const handleSignIn = useCallback(async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setError("");
     const data = new FormData(e.currentTarget);
     const email = data?.get("email");
     const password = data?.get("password");
-    console.log(email, password);
+
+    const url = BASE_URL + "/signin";
+
+    const user = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    }).then((res) => res.json());
+
+    if (!user.ok) {
+      setError(user?.message || "Unexpected error, try again later");
+      return;
+    }
+
+    console.log(user);
   }, []);
-  const handleSignUp = useCallback((e: FormEvent<HTMLFormElement>) => {
+  const handleSignUp = useCallback(async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setError("");
     const data = new FormData(e.currentTarget);
     const email = data?.get("email");
+    const username = data?.get("username");
     const password = data?.get("password");
-    console.log(email, password);
+
+    const url = BASE_URL + "/signup";
+
+    const user = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ email, username, password }),
+    }).then((res) => res.json());
+
+    if (!user.ok) {
+      setError(user?.message || "Unexpected error, try again later");
+      return;
+    }
+
+    console.log(user);
+  }, []);
+  const handleSwitchForm = useCallback((value) => {
+    setError("");
+    setSwitchForm(value);
   }, []);
 
   return (
     <GridMain>
       <Wrapper>
         <Heading>We are so glad having you here!</Heading>
-        <FormGroup>
-          <Form
-            handleSubmit={handleSignIn}
-            title="Welcome back"
-            callToAction="Login"
+        <RadioGroup>
+          <Radio
+            title="Login"
+            name="switchForm"
+            checked={switchForm === TypeOfForms.Login}
+            onChange={() => handleSwitchForm(TypeOfForms.Login)}
           />
-          <Form
-            handleSubmit={handleSignUp}
-            title="Create account"
-            callToAction="Register"
+          <Radio
+            title="Register"
+            name="switchForm"
+            checked={switchForm === TypeOfForms.Register}
+            onChange={() => handleSwitchForm(TypeOfForms.Register)}
           />
-        </FormGroup>
+        </RadioGroup>
+        <>
+          {switchForm === TypeOfForms.Login && (
+            <Form
+              handleSubmit={handleSignIn}
+              title="Welcome back"
+              callToAction="Login"
+            />
+          )}
+          {switchForm === TypeOfForms.Register && (
+            <Form
+              handleSubmit={handleSignUp}
+              title="Create account"
+              callToAction="Register"
+              hasUsername
+            />
+          )}
+        </>
+
+        {error && <ErrorMessage>{error}</ErrorMessage>}
       </Wrapper>
     </GridMain>
   );
--- apps/server/src/models/UserModel.ts
+++ apps/server/src/models/UserModel.ts
@@ -11,6 +11,7 @@ const User = mongoose.model(
     email: {
       type: String,
       unique: true,
+      required: true,
     },
     password: String,
   })
